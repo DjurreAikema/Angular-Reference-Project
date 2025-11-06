bbbcef36b8553ad361e606553059c02d07906383
d.aikema <djurreaikema@gmail.com>
2025-11-06 14:34:10 +0100

    Creating Checklist Items

--- src/app/checklist/ui/checklist-header.component.ts
+++ src/app/checklist/ui/checklist-header.component.ts
@@ -1,4 +1,4 @@
-import {Component, input} from '@angular/core';
+import {Component, input, output} from '@angular/core';
 import {Checklist} from '../../shared/interfaces';
 import {RouterLink} from '@angular/router';
 
@@ -10,10 +10,19 @@ import {RouterLink} from '@angular/router';
   template: `
     <header>
       <a routerLink="/home">Back</a>
-      <h1>{{ checklist().title }}</h1>
+
+      <h1>
+        {{ checklist().title }}
+      </h1>
+
+      <div>
+        <button (click)="addItem.emit()">Add item</button>
+      </div>
     </header>
   `
 })
 export class ChecklistHeaderComponent {
   checklist = input.required<Checklist>();
+
+  addItem = output();
 }
